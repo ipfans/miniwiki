3338d392136f2239676726f8e7cee13badfe1344
Dmitri Shuralyov <shurcooL@gmail.com>
2015-07-12 12:27:50 -0700

    Update import path of github_flavored_markdown package.
    It has moved out into a standalone repo recently. See https://github.com/shurcooL/go/issues/19#issuecomment-102574426 for rationale.

--- wikihelper/wikihelper.go
+++ wikihelper/wikihelper.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/shurcooL/go/github_flavored_markdown"
+	"github.com/shurcooL/github_flavored_markdown"
 )
 
 func UrlEncode(str string) (encoded string) {
